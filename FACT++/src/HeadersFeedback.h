a894b063764a13402c46b2975d4b2cc034c83c81
Dominik Neise <neised@phys.ethz.ch>
2017-07-31 16:14:31 +0200

    forgot to add header updates in the past

--- FACT++/src/HeadersFeedback.h
+++ FACT++/src/HeadersFeedback.h
@@ -1,8 +1,22 @@
 #ifndef FACT_HeadersFeedback
 #define FACT_HeadersFeedback
 
+#include "HeadersBIAS.h"
+
 namespace Feedback
 {
+    const int NumBiasChannels = 320;
+    const int NumberSix = 6;
+    const double DefaultOverVoltage = 1.1;
+    const double AlsoDefaultOverVoltage = 1.4;
+    const int NumberTwentyFive = 25;
+    const int ABrokenBoard = 272;
+
+    const double c1  =  1e-3 / 4096;
+    const double c5  =  5e-3 / 4096;
+    const double c10 = 10e-3 / 4096;
+
+
     namespace State
     {
         enum states_t
@@ -25,6 +39,23 @@ namespace Feedback
 
         };
     }
+
+    struct DimCurrents_t
+    {
+        float I[BIAS::kNumChannels];
+        float Iavg;
+        float Irms;
+        float Imed;
+        float Idev;
+        uint32_t N;
+        float Tdiff;
+        float Uov[BIAS::kNumChannels];
+        float Unom;
+        float dUtemp;
+
+        DimCurrents_t() { memset(this, 0, sizeof(dim_data)); }
+    } __attribute__((__packed__));
+
 }
 
 #endif
